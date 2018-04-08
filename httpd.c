bf27a17056f90706f0c6f30a9223ed0724410c9a
kingfisher <934313141@qq.com>
2018-04-08 18:13:35 +0800

    Create httpd.c

---
+++ httpd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int startup(u_short *);
+int get_line(int, char *, int);
+
+int main(void)
+{
+	
+}
