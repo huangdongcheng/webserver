e4f98f252707dfcc0e78de5c45bb1d556ecd52f7
kingfisher <934313141@qq.com>
2018-04-09 21:57:39 +0800

    Update httpd.c

--- httpd.c
+++ httpd.c
@@ -17,3 +17,32 @@ int main(void)
 {
 	
 }
+
+int get_line(int socket, char *buf, int size)
+{
+	int i = 0, n = 0;
+	char c = '\0';
+
+	while((i < size - 1) && (c != '\n')) {
+		n = recv(socket, &c, 1, 0);
+
+		if (n > 0) {
+			if (c == '\r') {
+				n = recv(socket, &c, 1, MSG_PEEK);
+				if (n > 0 && c == '\n') {
+					recv(socket, &c, 1, 0);
+				} else {
+					n = '\n';
+				}
+			}
+		} else {
+			c = '\n';
+		}
+		buf[i]= c;
+		i++;
+	}
+
+	buf[i] = '\0';
+
+	return n;
+}
